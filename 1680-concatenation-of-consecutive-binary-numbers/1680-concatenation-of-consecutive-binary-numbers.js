4cc5bd6ab74ee882c96620c7445b0e8aad473e33
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-17 19:25:18 +0200

    Time: 1930 ms (43.75%), Space: 47.7 MB (56.25%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var concatenatedBinary = function(n) {
+    let num = 0;
+    
+    for(let i = 1; i <= n; i++) {
+        num *= (2 ** i.toString(2).length) 
+        num += i;
+        num %= (10 ** 9 + 7)
+    }
+    return num;
+};
