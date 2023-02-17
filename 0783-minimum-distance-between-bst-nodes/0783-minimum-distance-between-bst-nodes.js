8769ab55d10e2755b171702a8131191dbf4d44ad
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-17 18:38:52 +0200

    Time: 61 ms (88.24%), Space: 42.3 MB (94.71%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function(root) {
+    let minDiff = Infinity;
+    let prev = null;
+    
+    function inorder(node) {
+        if (node == null) {
+            return;
+        }
+        
+        inorder(node.left);
+        
+        if (prev != null) {
+            minDiff = Math.min(minDiff, node.val - prev.val);
+        }
+        
+        prev = node;
+        
+        inorder(node.right);
+    }
+    
+    inorder(root);
+    
+    return minDiff;
+};
