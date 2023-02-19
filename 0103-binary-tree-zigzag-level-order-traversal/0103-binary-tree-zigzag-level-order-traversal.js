9776ce99e294f2a86670005e0c1e146a00ec0c58
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-19 10:38:14 +0200

    Time: 67 ms (74.48%), Space: 43.9 MB (56.89%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -10,17 +10,25 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function(root) {
-    const output = [];
-    const queue = [[root, true,0]]
-    while (queue.length) {
-      const [node, flip,level] = queue.shift();
-      if(!node) continue;
-      if(output[level] === undefined) output[level] = [];
-      if(!flip)output[level].push(node.val);
-      else output[level].unshift(node.val)
-      queue.push([node.right, !flip, level+1]);
-      queue.push([node.left, !flip, level+1]);
-    } 
-    return output;
-};
+function zigzagLevelOrder(root) {
+  let res = [];
+  go(root, 0, res);
+  return res;
+}
+
+function go(node, l, res) {  // l means level
+  if (!node) return;
+
+  if (res[l] == null) {
+    res.push([]);
+  }
+
+  if (l % 2 === 0) {
+    res[l].push(node.val);
+  } else {
+    res[l].unshift(node.val);
+  }
+
+  go(node.left, l + 1, res);
+  go(node.right, l + 1, res);
+}
