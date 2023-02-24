9f0cf41e99b01268512d7c9ae81f8e73b0b81440
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-24 14:25:58 +0200

    Time: 57 ms (92.60%), Space: 42.1 MB (20.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
-    var memo = {0:0,1:1}
+  var memo = {0:0,1:1}
   var helper = (x) => {
     if(memo[x] !== undefined) return memo[x]
     return memo[x] = helper(x-1) + helper(x-2)
