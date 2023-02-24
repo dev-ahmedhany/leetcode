9da8f6b8cbf2bcf98f64463869ff35bb9efcd16d
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-24 20:34:59 +0200

    Time: 54 ms (94.57%), Space: 41.8 MB (60.62%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+  let stack = [];
+    let num = 0;
+    let str = '';
+
+    for (let c of s) {
+        if (c === "[") {
+            stack.push(str);
+            stack.push(num);
+            num = 0;
+            str = '';
+        } else if (c === "]") {
+            let rep = stack.pop();
+            let prev = stack.pop();
+            str = prev + str.repeat(rep);
+        } else if (c >= "0" && c <= "9") {
+            num = num * 10 + parseInt(c);
+        } else {
+            str += c;
+        }
+    }
+
+    return str;
+};
