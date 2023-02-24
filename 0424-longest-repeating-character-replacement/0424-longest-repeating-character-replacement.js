1f7390f2b74f4db17804b9cbf522bc3726ba9256
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-24 15:28:03 +0200

    Time: 84 ms (74.71%), Space: 42.4 MB (77.60%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const characterReplacement = (s, k) => {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
+
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+
+  return right - left;
+};
