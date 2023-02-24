e5be62173c1c61e4533ffa4dd372446c8056c718
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-02-24 15:42:06 +0200

    Time: 68 ms (88.56%), Space: 44.5 MB (55.51%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function(secret, guess) {
+  var map = {};
+  var A = 0;
+  var B = 0;
+  for (var i = 0; i < 10; i++) map[i] = 0;
+  for (i = 0; i < secret.length; i++) {
+    if (secret[i] === guess[i]) A++;
+    else {
+      map[secret[i]]++;
+      B += map[secret[i]] <= 0 ? 1 : 0;
+      map[guess[i]]--;
+      B += map[guess[i]] >= 0 ? 1 : 0;
+    }
+  }
+  return A + 'A' + B + 'B';
+};
