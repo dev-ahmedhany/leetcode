ca44a917f5311afdb8bd3f94fd329e006b1b2c57
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-03-14 10:56:28 +0200

    Time: 130 ms (44.71%), Space: 51.1 MB (7.51%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {    
+    // Use min heap to keep track of the smallest node in constant time.
+    // Enqueue and dequeue will be log(k) where k is the # of lists
+    // b/c we only need to keep track of the next node for each list
+    // at any given time.
+    const minHeap = new MinPriorityQueue({ priority: item => item.val });
+    
+    for (let head of lists) {
+        if (head) minHeap.enqueue(head);
+    }
+    
+    // Create tempHead that we initiate the new list with
+    // Final list will start at tempHead.next
+    const tempHead = new ListNode();
+    let curr = tempHead;
+    
+    while (!minHeap.isEmpty()) {
+        const { val, next } = minHeap.dequeue().element;
+        curr.next = new ListNode(val);
+        curr = curr.next;
+        
+        if (next) minHeap.enqueue(next);
+    }
+    
+    return tempHead.next;
+};
