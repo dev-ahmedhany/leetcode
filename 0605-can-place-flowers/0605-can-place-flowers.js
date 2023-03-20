afa1d22a6af3f27e6ecea144f36807dfcf42b9f3
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-03-20 17:51:11 +0200

    Time: 63 ms (82.22%), Space: 44.6 MB (28.72%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
+    }
+  }
+  return n === 0;
+};
