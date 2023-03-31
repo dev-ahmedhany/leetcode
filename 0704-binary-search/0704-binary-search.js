2c2d3e41d834080eefb6aa61144015b034a4d572
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-03-31 12:40:02 +0200

    Time: 66 ms (60.47%), Space: 45.2 MB (26.92%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,10 +3,10 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    var start =0,end = nums.length -1
+const search = function(nums, target) {
+    let start =0,end = nums.length -1
     while(end >= start){
-        var mid = Math.floor((end + start)/2)
+        const mid = Math.floor((end + start)/2)
         if(nums[mid] === target){
             return mid
         } else if (nums[mid] > target){
