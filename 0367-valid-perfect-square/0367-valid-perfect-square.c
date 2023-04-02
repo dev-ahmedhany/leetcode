92fdf3ad1a17c13ef2a4f574d28b4a507b6a22d3
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-04-02 22:38:15 +0200

    Time: 3 ms (50.32%), Space: 5.6 MB (45.67%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,17 +1,7 @@
 bool isPerfectSquare(int num){
-    int start = 1, end = num >> 1 ,mid;
-    
-    if(num == 1) return true;
-    
-    while(end >= start){
-        mid = start + ((end - start) >> 1);
-        if(num / mid == mid && mid * mid == num){
-            return true;
-        } else if(num / mid > mid){
-            start = mid +1;
-        } else {
-            end = mid - 1;
-        }
+    long r = num;
+    while(r*r > num){
+        r = (r + num/r) /2;
     }
-    return false;
+    return r*r == num;
 }
