899935f581fe2ac38932c9fc164fd3b529cb9ec2
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-04-02 22:51:27 +0200

    Time: 8 ms (80.39%), Space: 6.3 MB (29.41%) - LeetHub

---
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.c
@@ -0,0 +1,14 @@
+int findTheDistanceValue(int* arr1, int arr1Size, int* arr2, int arr2Size, int d){
+    int n=0, count=0, i;
+    for(i=0; i<arr1Size; i++){
+        count++;
+        for(int j=0; j<arr2Size; j++){
+            n=abs(arr1[i]-arr2[j]);
+            if(n<= d){
+                count--;
+                break;
+            }
+        }
+    }
+    return count;
+}
