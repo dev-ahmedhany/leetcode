e5b8734d3afd027f15897aa6d00bd754663a29af
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-04-11 13:05:04 +0200

    Time: 12 ms (50.35%), Space: 6 MB (56.03%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.c
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.c
@@ -1,7 +1,7 @@
 char nextGreatestLetter(char* letters, int lettersSize, char target){
     int left = 0, right = lettersSize - 1;
     while (left < right){
-        int mid = left + ((right-left) >> 2);
+        int mid = left + ((right-left) >> 1);
         if (letters[mid] <= target)
             left = mid + 1;
         else
