d62598ffcb6bd12d2cfd54a671bf254ff85b73ed
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-04-17 17:29:30 +0200

    Time: 65 ms (46.56%), Space: 42.1 MB (75.21%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies);
+    return candies.map(candy => candy + extraCandies >= max);
+};
