88682d410f3e998cca8d21623dafdf4133b8a5e2
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-05-10 00:37:28 +0300

    Time: 44 ms (98.80%), Space: 42 MB (34.50%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    const res = []
+    while(matrix.length){
+    const first = matrix.shift()
+    res.push(...first)
+    for(const m of matrix){
+      let val = m.pop()
+      if(val)
+        res.push(val)
+        m.reverse()   
+    }
+    matrix.reverse()
+    }
+    return res
+};
