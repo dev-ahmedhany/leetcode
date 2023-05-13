3c7429fbb520231e96337c18abb8d75322ae576b
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-05-13 19:00:50 +0300

    Time: 54 ms (81.97%), Space: 41.8 MB (62.18%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let hasBeenCalled = false;
+    return function(...args){
+        if (hasBeenCalled) {
+          return undefined;
+        } else {
+          hasBeenCalled = true;
+          return fn(...args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
