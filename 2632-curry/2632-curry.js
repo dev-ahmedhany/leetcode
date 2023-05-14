b6a78264f2b94f957ffd5655e6bf49c4b6b9a63d
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-05-15 02:30:09 +0300

    Time: 66 ms (71.62%), Space: 48.4 MB (45.96%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    return function curried(...args) {
+      if(args.length >= fn.length) {
+         return fn(...args);
+      }
+
+      return (...nextArgs) => curried(...args, ...nextArgs);
+   };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
