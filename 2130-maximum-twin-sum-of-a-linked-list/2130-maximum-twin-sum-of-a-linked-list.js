7bbe399af924f578ad50f229d403e4a6701f3616
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-05-17 09:44:34 +0300

    Time: 182 ms (42.07%), Space: 93.2 MB (10.70%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    let st = []; 
+    while(head != null){
+        st.push(head.val);
+        head = head.next;
+    }
+    let max = 0;
+    for(let i=0; i<st.length; i++){
+        max = Math.max(max, (st[i] + st[st.length-1-i]));
+    }
+    return max;
+};
