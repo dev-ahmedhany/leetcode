6dd4371afcaee451e6d35fd46c078c68246d0eef
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-06-02 15:39:36 +0300

    Time: 49 ms (98.34%), Space: 44.5 MB (50.29%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+  return Array.from({ length: Math.ceil(arr.length / size) }, function(_, index) {
+    return arr.slice(index * size, index * size + size);
+  });
+};
