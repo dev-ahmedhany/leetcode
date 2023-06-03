7efba77e68aec7b3896a91ac2e31f3c6db076d82
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-06-03 17:23:59 +0300

    Time: 213 ms (57.80%), Space: 74.8 MB (45.65%) - LeetHub

---
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+    if(!Array.isArray(arr)) {
+        yield arr
+        return
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        yield* inorderTraversal(arr[i])
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
