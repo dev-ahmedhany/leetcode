58aae697dcd72170e4d224bb0b37f151160fb09f
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-06-30 11:28:46 +0300

    Time: 62 ms, Memory: 45 MB - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != nums[i]) 
+            nums[++i] = nums[j];
+    }
+    return ++i;
+};
