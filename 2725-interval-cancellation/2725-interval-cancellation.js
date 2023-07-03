9eca8806218d103ccdb9349bd776ef97d0b43c45
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-07-03 20:51:23 +0300

    Time: 75 ms (35.29%), Space: 41.9 MB (76.56%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    const x = setInterval(() => fn(...args),t)
+    const cancelFn = () => clearInterval(x)
+    return cancelFn
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *		const val = fn(...argsArr)
+ *      result.push({"time": Math.floor(performance.now() - start), "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},           
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT)
+ */
