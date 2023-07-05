9af2e4f968854ff1eca9fe99d1e110a246a46b83
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-07-05 19:32:12 +0300

    Time: 58 ms, Memory: 44.3 MB - LeetHub

--- sort-colors/sort-colors.ts
+++ sort-colors/sort-colors.ts
@@ -2,5 +2,25 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-    nums.sort((a,b)=> a-b)
+    function swap(i, j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    let l = 0;
+    let r = nums.length - 1;
+    let i = 0;
+
+    while (i <= r) {
+        const n = nums[i];
+        if (n === 0) {
+            swap(i, l);
+            l++;
+            i++;
+        } else if (n === 2) {
+            swap(i, r);
+            r--;
+        } else {
+            i++;
+        }
+    }
 };
