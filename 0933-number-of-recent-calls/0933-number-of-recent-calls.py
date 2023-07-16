749b5954a479cb1cabb0e3fccb0f9e23b0fa9131
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-07-16 10:19:06 +0300

    Time: 293 ms (24.70%), Space: 21.3 MB (99.14%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,18 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.slide_window = deque()
+
+    def ping(self, t: int) -> int:
+        # step 1). append the current call
+        self.slide_window.append(t)
+
+        # step 2). invalidate the outdated pings
+        while self.slide_window[0] < t - 3000:
+            self.slide_window.popleft()
+
+        return len(self.slide_window)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
