74a9496bfe44eafae44ce9163abf5bfe8c0eb0b9
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-07-17 11:41:48 +0300

    Time: 50 ms (48.04%), Space: 16.5 MB (27.96%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -11,36 +11,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        leafs1 = self.leafs(root1)
-        leafs2 = self.leafs(root2)
-        while True:
-            try:
-                val1 = next(leafs1)
-                done1 = True
-            except:
-                done1 = False
+    def leafSimilar(self, root1, root2):
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
 
-            try:
-                val2 = next(leafs2)
-                done2 = True
-            except:
-                done2 = False
-
-            if done1 and done2:
-                if val1 != val2:
-                    return False
-            elif not done1 and not done2:
-                return True
-            else:
-                return False
-
-
-    def leafs(self, node):
-        if not node.left and not node.right:
-            yield node.val
-        else:
-            if node.left:
-                yield from self.leafs(node.left)
-            if node.right:
-                yield from self.leafs(node.right)
+        return list(dfs(root1)) == list(dfs(root2))
